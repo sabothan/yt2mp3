83c5ee98f79f4c3c212a34d6e4ef6df7f8f48d04
Jonathan Seeber <seeber.joni@outlook.de>
2025-05-15 14:29:43 +0200

    CLI

---
+++ yt2mp3/main.py
@@ -0,0 +1,13 @@
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description='Convert YouTube videos to MP3.')
+    parser.add_argument('--url', required=True, help='The URL to the YouTube video.')
+    parser.add_argument('--path', required=False, default='~/Downloads', help='The target folder for the download.')
+
+    args = parser.parse_args()
+
+    print('Welcome to yt2mp3')
+
+if __name__ == '__main__':
+    main()
